f190f7505f11d903ca3294633025e966c1c1e8ce
KIM34000 <lina7599@hotmail.com>
2022-01-04 10:47:44 +0100

    test

--- src/initiation/MainClassProject.java
+++ src/initiation/MainClassProject.java
@@ -1,7 +1,16 @@
 package initiation;
 
+import javax.sql.rowset.JoinRowSet;
+
 public class MainClassProject {
 	public static void main(String[]args) {
-		System.out.println ("bonjour");
+	
+//		{
+////		System.out.println("Un");
+////		System.out.println("Deux");
+////		System.out.println("Trois");
+//		}
+//		System.out.println("Exit");
+		
 	}
 }
